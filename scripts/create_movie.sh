66cc386a1c5faff54874424b92a13b8e812ca9bb
NorthernMan54 <buyminivan-northern@yahoo.ca>
2020-09-15 22:35:00 -0400

    Allow only single instance of create_movie to run at the same time

--- scripts/create_movie.sh
+++ scripts/create_movie.sh
@@ -8,15 +8,17 @@ if [[ "$OS" == 'Darwin' ]]; then
   # OPTIONS=
   echo "Mac"
 elif [[ "$OS" == 'Linux' ]]; then
-  # OPTIONS="-c:v h264_omx -profile:v high -b:v 3000000"
+  INPUT="-c:v h264_mmal"
   OPTIONS="-c:v h264_omx -b:v 3000000"
   echo "Linux"
 fi
 
-(cd /var/tmp
-#rm *mp4
+(
+flock -e 200
+cd /var/tmp
 ls -tr ${INSTANCE}/* | tail -61 | head -60 | awk '{ print "file "$1 }' > ${INSTANCE}.txt
-#ffmpeg -f concat -c:v h264_mmal -i ${INSTANCE}.txt -c:v h264_omx -profile:v high -b:v 3000000 -y ${FILENAME}
-timeout --signal=9 180s ~/npm/lib/node_modules/ffmpeg-for-homebridge/ffmpeg -hide_banner -loglevel error -f concat -i ${INSTANCE}.txt \
-${OPTIONS} -vf "mpdecimate=hi=10000:lo=600:frac=0.1,setpts=N/(15*TB)" -y ${FILENAME}
-)
+
+~/npm/lib/node_modules/ffmpeg-for-homebridge/ffmpeg -hide_banner -loglevel error \
+-f concat ${INPUT} -i ${INSTANCE}.txt ${OPTIONS} \
+-vf "mpdecimate=hi=10000:lo=600:frac=0.1,setpts=N/(15*TB)" -y ${FILENAME}
+) 200>/tmp/create_movie.lockfile
